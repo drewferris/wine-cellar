ac71d16d1a049e6b927b27c681662c8da6577ff9
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-07-28 19:40:36 -0700

    wrote basic server.js, copied over gulpfile.js, added db to .gitignore, updated package.json

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 'use strict';
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const dbPort = process.env.MONGODB_URI || 'mongodb://localhost/dev_db';
+
+mongoose.connect(dbPort);
 
 app.use(express.static(__dirname + '/build'));
 
-app.listen(8080, () => {
-  console.log('server is running on 8080');
+app.use((req, res) => {
+  console.log('hit end');
+  res.status(404).json({message: 'route not found'});
+}).use((err, req, res, next) => {
+  res.status(500).json({message: err.message});
+  next(err);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('listening on' + (process.env.PORT || 3000));
 });
