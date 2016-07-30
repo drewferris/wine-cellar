b08b78621bfe29c888fbeb458cfe28e498adaa8b
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-07-30 09:46:16 -0700

    wrote signin get route

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -20,3 +20,11 @@ router.post('/signup', bodyParser, (req, res, next) => {
     });
   });
 });
+
+router.get('/signin', basicHTTP, (req, res, next) => {
+  User.findOne({username: req.auth.username}, (err,user) => {
+    if (err || !user) return next(new Error('Could not login'));
+    if (!user.comparePassword(req.auth.passpassword)) return next('Could not sign in');
+    res.json({token: user.generateToken()});
+  });
+});
