11c66be9cc9d7a3978204c6f31214cb841bd91f0
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-08-01 15:42:07 -0700

    added chai and chai-http to package.json and wrote the get bottle route test

---
+++ test/bottle_route.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
+chai.use(chaiHTTP);
+const expect = chai.expect;
+const request = chai.request;
+const mongoose = require('mongoose');
+const dbPort = process.env.MONGOLAB_URI;
+let testToken = '';
+
+process.env.MONGOLAB_URI = 'mongodb://localhost/test_db';
+require('../server.js');
+
+describe('unit tests for CRUD bottle routes', () => {
+  before((done) => {
+    request('localhost:3000')
+    .post('/auth/signup')
+    .send({username: 'user', password: 'password'})
+    .end((err, user) => {
+      testToken = user.body.token;
+      if (err) console.log('error message');
+      done();
+    });
+  });
+  after((done) => {
+    process.env.MONGOLAB_URI = dbPort;
+    mongoose.connection.db.dropDatabase(() => {
+      done();
+    });
+  });
+  it('should get a list of bottles', (done) => {
+    request('localhost:3000')
+      .get('/bottle')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(Array.isArray(res.body)).to.be.true;
+        done();
+      });
+  });
+});
