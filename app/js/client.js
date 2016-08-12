cccf8d2c04a6fdda83c1ed17fef6cb6b141c9961
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-08-11 20:56:33 -0700

    made frontend start to work finally

--- app/js/client.js
+++ app/js/client.js
@@ -1,8 +1,18 @@
 'use strict';
 
 const angular = require('angular');
-const app = angular.module('CellarApp', []);
+const ngRoute = require('angular-route');
+
+const app = angular.module('CellarApp', [ngRoute]);
 
 require('./services')(app);
 require('./controllers')(app);
 require('./directives')(app);
+
+app.config(function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: './templates/partials/cellar.html',
+    controller: 'BottleController',
+    controllerAs: 'botlctrl'
+  });
+});
---
+++ app/js/controllers/index.js
@@ -0,0 +1,3 @@
+module.exports = function(app) {
+  require('./bottle_controller')(app);
+};
